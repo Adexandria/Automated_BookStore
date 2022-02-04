bcdde0b0188732e4fa4bffbd057f434a78f7f58a
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-02-04 14:06:23 +0100

    dbcontext

---
+++ Authentication/Authentication.Infrastructure/Service/AuthDbService.cs
@@ -0,0 +1,19 @@
+﻿using Authentication.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Authentication.Infrastructure.Service
+{
+    public class AuthDbService :DbContext
+    {
+        public AuthDbService(DbContextOptions<AuthDbService> options ):base(options)
+        {
+
+        }
+        public DbSet<SignUp> Users { get; set; }
+        public DbSet<Faculty> Faculties { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
