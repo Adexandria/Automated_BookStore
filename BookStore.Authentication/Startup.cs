6b0ebcad8691e1c53f1c2739e68ba00ab552e787
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-02-04 09:29:18 +0100

    Add project files.

---
+++ BookStore.Authentication/Startup.cs
@@ -0,0 +1,63 @@
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BookStore.Authentication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            services.AddIdentityServer().AddConfigurationStore(option =>
+            {
+                option.ConfigureDbContext = b => b.UseSqlServer(Configuration["Authentication"], sql => sql.MigrationsAssembly(migrationsAssembly));
+            }).AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = b => b.UseSqlServer(Configuration["Authentication"],
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
