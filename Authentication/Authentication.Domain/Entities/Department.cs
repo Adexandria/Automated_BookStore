d23b453abc12536cd386e56872317a7374ce0a90
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-02-04 14:05:37 +0100

    authentication entities

---
+++ Authentication/Authentication.Domain/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Authentication.Domain.Entities
+{
+    public class Department
+    {
+        [Key]
+        public Guid DepartmentId { get; set; }
+        public string Deparment { get; set; }
+
+        [ForeignKey("FacultyId")]
+        public Guid FacultyId { get; set; }
+        public Faculty Faculty { get; set; }
+    }
+}
---
+++ Authentication/Authentication.Domain/Entities/Faculty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Authentication.Domain.Entities
+{
+    public class Faculty
+    {
+        [Key]
+        public Guid FacultyId { get; set; }
+        public string FacultyName { get; set; }
+ 
+    }
+}
