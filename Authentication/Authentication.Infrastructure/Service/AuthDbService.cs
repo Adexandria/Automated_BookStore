cffbcf3f58f694bdb86522e5b0f7311d50204e41
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-02-11 12:34:12 +0100

    added identity db context

--- Authentication/Authentication.Infrastructure/Service/AuthDbService.cs
+++ Authentication/Authentication.Infrastructure/Service/AuthDbService.cs
@@ -1,18 +1,21 @@
 ﻿using Authentication.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Configuration;
 
 namespace Authentication.Infrastructure.Service
 {
-    public class AuthDbService :DbContext
+    public class AuthDbService :IdentityDbContext<SignUp>
     {
-        public AuthDbService(DbContextOptions<AuthDbService> options ):base(options)
+        public AuthDbService(DbContextOptions<AuthDbService> options):base(options)
         {
-
+  
         }
-        public DbSet<SignUp> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public override DbSet<SignUp> Users { get; set; }
         public DbSet<Faculty> Faculties { get; set; }
         public DbSet<Department> Departments { get; set; }
     }
