bf4c7bd412dae0fd56c958ae1874ad1216d64e77
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-02-16 12:57:07 +0100

    mail model

---
+++ Authentication/Authentication.Domain/Entities/Mail.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Authentication.Domain.Entities
+{
+    public class Mail
+    {
+        public string  To { get; set; }
+        public string Subject { get; set; } = "Verify your student email";
+        public string Text { get; set; }
+    }
+}
