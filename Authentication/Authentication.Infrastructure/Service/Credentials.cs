4bda7a2399feda36c5b0038e6a0f4f45cf7740f3
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-02-19 08:23:26 +0100

    jwt service

---
+++ Authentication/Authentication.Infrastructure/Service/Credentials.cs
@@ -0,0 +1,60 @@
+﻿using Authentication.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authentication.Infrastructure.Service
+{
+    public class Credentials
+    {
+        readonly IConfiguration _config;
+        readonly UserManager<SignUp> _userManager;
+        public Credentials(IConfiguration _config, UserManager<SignUp> _userManager)
+        {
+            this._config = _config;
+            this._userManager = _userManager;
+        }
+        private IConfigurationSection GetSection()
+        {
+            return _config.GetSection("JwtSettings");
+        }
+
+        public SigningCredentials GetSigningCredentials()
+        {
+            var _jwtSettings = GetSection();
+            var key = Encoding.UTF8.GetBytes(_jwtSettings.GetSection("securityKey").Value);
+            var secret = new SymmetricSecurityKey(key);
+            return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+        }
+        public JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials, List<Claim> claims)
+        {
+            var _jwtSettings = GetSection();
+            var tokenOptions = new JwtSecurityToken(
+            issuer: _jwtSettings.GetSection("validIssuer").Value,
+            audience: _jwtSettings.GetSection("validAudience").Value,
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(Convert.ToDouble(_jwtSettings.GetSection("expiryInMinutes").Value)),
+            signingCredentials: signingCredentials);
+            return tokenOptions;
+        }
+        public async Task<List<Claim>> GetClaims(SignUp user)
+        {
+            var claims = new List<Claim>
+            {
+            new Claim(ClaimTypes.Name, user.Email)
+            };
+            var roles = await _userManager.GetRolesAsync(user);
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+            return claims;
+        }
+    }
+}
