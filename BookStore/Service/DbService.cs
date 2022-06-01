6e8f3eda4c50cd44a1e0fc3a428d5c52e1a6bf6f
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-01 16:10:34 +0100

    created dbservice for bookstore

---
+++ BookStore/Service/DbService.cs
@@ -0,0 +1,26 @@
+﻿using Bookstore.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Service
+{
+    public class DbService : DbContext
+    {
+        public DbService(DbContextOptions<DbService> options):base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<BookCategory> BookCategories { get; set; }
+        public DbSet<BookDetail> BookDetails { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderCart> Carts { get; set; }
+
+    }
+}
