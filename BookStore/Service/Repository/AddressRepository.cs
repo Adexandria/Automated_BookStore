f429f92eccc05006709fa76db0ce82961943b995
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-02 15:05:38 +0100

    address implementation

---
+++ BookStore/Service/Repository/AddressRepository.cs
@@ -0,0 +1,77 @@
+﻿using Bookstore.Model;
+using Bookstore.Service.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Service.Repository
+{
+    public class AddressRepository : IAddress
+    {
+        readonly DbService db;
+        public AddressRepository(DbService db)
+        {
+            this.db = db;
+        }
+        public async Task<int> AddAddresss(Address address, Guid profileId)
+        {
+            try
+            {
+                address.AddressId = Guid.NewGuid();
+                address.ProfileId = profileId;
+                await db.Addresses.AddAsync(address);
+                return await SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+        }
+
+        public async Task<int> DeleteAddressById(Guid addressId)
+        {
+            try
+            {
+                Address currentAddress = await GetAddress(addressId);
+                db.Addresses.Remove(currentAddress);
+                return await SaveChanges();
+            }
+            catch ( Exception ex)
+            {
+
+                throw ex;
+            }
+           
+
+        }
+
+        public async Task<int> UpdateAddress(Address address)
+        {
+            try
+            {
+                Address currentAddress = await GetAddress(address.AddressId);
+                db.Entry(currentAddress).CurrentValues.SetValues(address);
+                db.Entry(currentAddress).State = EntityState.Modified;
+                return await SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        private async Task<Address> GetAddress(Guid addressId)
+        {
+            return await db.Addresses.Where(s => s.AddressId == addressId).FirstOrDefaultAsync(); 
+        }
+        private async Task<int> SaveChanges()
+        {
+            return await db.SaveChangesAsync();
+        }
+    }
+}
