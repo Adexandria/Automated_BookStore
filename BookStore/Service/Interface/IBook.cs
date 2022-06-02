fdc7b9c6288a9f1986d61d0a2ba9e553634873ae
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-02 15:04:49 +0100

    interface for book

--- BookStore/Service/Interface/IBook.cs
+++ BookStore/Service/Interface/IBook.cs
@@ -2,17 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Bookstore.Model;
-public interface IBook
+
+namespace Bookstore.Service.Interface
 {
-    Task<Book> GetBookById(Guid bookId);
-    IEnumerable<Book> GetBooks{get;}
-    IEnumerable<Book> GetBookByName(string bookName);
-    Task<Book>GetBookByISBN13(string isbn);
-    IEnumerable<Book>GetBooksByFaculty(string faculty);
-    IEnumerable<Book> GetBooksByLevel(string faculty ,int level);
-    IEnumerable<Book> GetBooksByAuthor(string author);
-    Task<int> AddBook(Book book);
-    Task<int> UpdateBook(Book updatedBook);
-    Task<int> DeleteBookById(Guid bookId);
- 
+    public interface IBook
+    {
+        Task<Book> GetBookById(Guid bookId);
+        IEnumerable<Book> GetBooks { get; }
+        IEnumerable<Book> GetBookByName(string bookName);
+        Task<Book> GetBookByISBN13(string isbn);
+        IEnumerable<Book> GetBooksByFaculty(string faculty);
+        IEnumerable<Book> GetBooksByLevel(string faculty, int level);
+        IEnumerable<Book> GetBooksByAuthor(string author);
+        Task<int> AddBook(Book book);
+        Task<int> UpdateBook(Book updatedBook);
+        Task<int> DeleteBookById(Guid bookId);
+
+    }
 }
