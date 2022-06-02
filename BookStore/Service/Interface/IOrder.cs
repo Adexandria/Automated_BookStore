823c232f972500b4ab09a58a7d6f27139fe086dc
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-02 15:04:18 +0100

    interface for order

---
+++ BookStore/Service/Interface/IOrder.cs
@@ -0,0 +1,21 @@
+﻿using Bookstore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Service.Interface
+{
+    public interface IOrder
+    {
+        IEnumerable<Order> GetUserOrders(Guid profileId);
+        IEnumerable<Order> GetOrdersByStatus(string status);
+        Task<Order> GetOrdersByTrackingNumber(Guid trackingNumber);
+
+        Task<int> AddOrder(Order order);
+        Task<int> UpdateOrder(Order order);
+        Task<int> DeleteOrderById(Guid trackingNumber);
+        
+
+    }
+}
