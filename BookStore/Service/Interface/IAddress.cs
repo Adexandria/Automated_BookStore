2e91bd34e370a1492a5f3d5f094f795a59c554b4
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-02 15:05:21 +0100

    interface for address

---
+++ BookStore/Service/Interface/IAddress.cs
@@ -0,0 +1,15 @@
+﻿using Bookstore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Service.Interface
+{
+    public interface IAddress
+    {
+        Task<int> AddAddresss(Address address, Guid profileId);
+        Task<int> UpdateAddress(Address address);
+        Task<int> DeleteAddressById(Guid addressId);
+    }
+}
