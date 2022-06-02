4677332b561e15a604e7d8e1918267195b3497af
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-02 15:04:35 +0100

    interface for book detail

--- BookStore/Service/Interface/IDetail.cs
+++ BookStore/Service/Interface/IDetail.cs
@@ -2,9 +2,12 @@ using System;
 using System.Threading.Tasks;
 using Bookstore.Model;
 
-public interface IDetail
+namespace Bookstore.Service.Interface
 {
-    Task<int> AddDetail(BookDetail detail);
-    Task<int> UpdateDetail(BookDetail updatedDetail);
-    Task<int> DeleteDetailById(Guid detailId);
+    public interface IDetail
+    {
+        Task<int> AddDetail(BookDetail detail);
+        Task<int> UpdateDetail(BookDetail updatedDetail);
+        Task<int> DeleteDetailById(Guid detailId);
+    }
 }
