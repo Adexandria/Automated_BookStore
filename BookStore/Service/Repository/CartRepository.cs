931405dc77b176e134667de459fe88a9581eebdf
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-03 18:29:57 +0100

    created repository for order cart

---
+++ BookStore/Service/Repository/CartRepository.cs
@@ -0,0 +1,76 @@
+﻿using Bookstore.Model;
+using Bookstore.Service.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Service.Repository
+{
+    public class CartRepository : ICart
+    {
+        readonly DbService db;
+        public CartRepository(DbService db)
+        {
+            this.db = db;
+        }
+        public async Task<int> AddToCart(OrderCart cart)
+        {
+            try
+            {
+                cart.CartId  = Guid.NewGuid();
+                await db.Carts.AddAsync(cart);
+                return await SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<int> DeleteFromCart(Guid cartId)
+        {
+            try
+            {
+                OrderCart currentCart = await GetCart(cartId);
+                db.Carts.Remove(currentCart);
+                return await SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public IEnumerable<OrderCart> GetOrdersCart(Guid orderId)
+        {
+            return db.Carts.Where(s => s.OrderId == orderId).Include(s => s.Book).OrderBy(s => s.BookId);
+        }
+        public async Task<int> UpdateCart(OrderCart cart)
+        {
+            try
+            {
+                OrderCart currentCart = await GetCart(cart.CartId);
+                db.Entry(currentCart).CurrentValues.SetValues(cart);
+                db.Entry(currentCart).State = EntityState.Modified;
+                return await SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        private async Task<OrderCart> GetCart(Guid cartId)
+        {
+            return await db.Carts.Where(s => s.CartId == cartId).FirstOrDefaultAsync();
+        }
+        private async Task<int> SaveChanges()
+        {
+            return await db.SaveChangesAsync();
+        }
+    }
+}
