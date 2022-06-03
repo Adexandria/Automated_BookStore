0f23481d0d32e53873571e6cc3575e3087136d89
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-03 18:30:09 +0100

    repository for order

---
+++ BookStore/Service/Repository/OrderRepository.cs
@@ -0,0 +1,88 @@
+﻿using Bookstore.Model;
+using Bookstore.Service.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Service.Repository
+{
+    public class OrderRepository : IOrder
+    {
+        readonly DbService db;
+        public OrderRepository(DbService db)
+        {
+            this.db = db;
+        }
+        public async Task<int> AddOrder(Order order)
+        {
+            try
+            {
+                order.OrderId = Guid.NewGuid();
+                await db.Orders.AddAsync(order);
+                return await SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+      
+
+        public IEnumerable<Order> GetOrdersByStatus(string status)
+        {
+            return db.Orders.Where(s => s.Status.ToString() == status).OrderBy(s=>s.OrderId);
+        }
+
+        public async Task<Order> GetOrdersByTrackingNumber(Guid trackingNumber)
+        {
+            return await db.Orders.Where(s => s.OrderId == trackingNumber).FirstOrDefaultAsync();
+        }
+
+        public IEnumerable<Order> GetUserOrders(Guid profileId)
+        {
+            return db.Orders.Where(s => s.ProfileId == profileId).OrderBy(s => s.OrderId);
+        }
+
+        public async Task<int> UpdateOrder(Order order)
+        {
+            try
+            {
+                Order currentOrder = await GetOrdersByTrackingNumber(order.OrderId);
+                db.Entry(currentOrder).CurrentValues.SetValues(order);
+                db.Entry(currentOrder).State = EntityState.Modified;
+                return await SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+
+        }
+
+        public async Task<int> DeleteOrderById(Guid trackingNumber)
+        {
+            try
+            {
+                Order currentOrder = await GetOrdersByTrackingNumber(trackingNumber);
+                db.Orders.Remove(currentOrder);
+                return await SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        private async Task<int> SaveChanges()
+        {
+            return await db.SaveChangesAsync();
+        }
+    }
+}
