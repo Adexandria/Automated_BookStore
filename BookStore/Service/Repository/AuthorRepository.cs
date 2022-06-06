7c724efca1dd65be3cd724f27478f2de7b9deb2d
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-06 08:42:41 +0100

    repository of author

---
+++ BookStore/Service/Repository/AuthorRepository.cs
@@ -0,0 +1,73 @@
+﻿using Bookstore.Model;
+using Bookstore.Service.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Service.Repository
+{
+    public class AuthorRepository : IAuthor
+    {
+        readonly DbService db;
+        public AuthorRepository(DbService db)
+        {
+            this.db = db;
+        }
+        public async Task<int> AddAuthor(Author author)
+        {
+            try
+            {
+                author.AuthorId = Guid.NewGuid();
+                await db.Authors.AddAsync(author);
+                return await SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<int> DeleteAuthorById(Guid authorId)
+        {
+            try
+            {
+                Author currentAuthor = await GetAuthor(authorId);
+                db.Authors.Remove(currentAuthor);
+                return await SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public async Task<int> UpdateAuthor(Author updatedAuthor)
+        {
+            try
+            {
+                Author currentAuthor = await GetAuthor(updatedAuthor.AuthorId);
+                db.Entry(currentAuthor).CurrentValues.SetValues(updatedAuthor);
+                db.Entry(currentAuthor).State = EntityState.Modified;
+                return await SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        private async Task<Author> GetAuthor(Guid authorId)
+        {
+            return await db.Authors.Where(s => s.AuthorId == authorId).FirstOrDefaultAsync();
+        }
+        private async Task<int> SaveChanges()
+        {
+            return await db.SaveChangesAsync();
+        }
+    }
+}
