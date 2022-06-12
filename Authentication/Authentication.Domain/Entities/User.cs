72af42cb3f0732f57cc5bd6e7bf83ad7ef3a76e6
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-12 13:02:52 +0100

    identity user class

---
+++ Authentication/Authentication.Domain/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Authentication.Domain.Entities
+{
+    public class User:IdentityUser
+    {
+
+    }
+}
