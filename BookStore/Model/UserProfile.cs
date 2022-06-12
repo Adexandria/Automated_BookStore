22219c775b8972af20215d5bfcf9848c57d57a07
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-12 13:06:25 +0100

    added user profile

--- BookStore/Model/UserProfile.cs
+++ BookStore/Model/UserProfile.cs
@@ -11,6 +11,13 @@ namespace Bookstore.Model
     {
         [Key]
         public Guid ProfileId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string MatriculationNumber { get; set; }
+        public string Faculty { get; set; }
+        public string Department { get; set; }
+        public int Level { get; set; }
         public string Id { get; set; }
        
     }
