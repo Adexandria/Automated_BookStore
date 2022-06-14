e210eda3fa0acc801755f5e0ddefb18316a51771
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-14 21:54:57 +0100

    Detail create object

---
+++ BookStore/Model/DTO/Detail/DetailCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Model.DTO.Detail
+{
+    public class DetailCreate
+    {
+        public string ISBN13 { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int Pages { get; set; }
+        public int Rating { get; set; }
+        public int Sales { get; set; }
+        public int Quantity { get; set; }
+        public string Description { get; set; }
+    }
+}
