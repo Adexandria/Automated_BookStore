265a958a705318427a1b47b5a2730187a1eb4ce6
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-15 11:05:55 +0100

    created dto for book

---
+++ BookStore/Model/DTO/Book/BookCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Model.DTO.Book
+{
+    public class BookCreate
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Picture { get; set; }
+        public string BookLink { get; set; }
+        public string Department { get; set; }
+        public int Level { get; set; }
+        public string ISBN { get; set; }
+    }
+}
