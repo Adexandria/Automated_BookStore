e5d07acd9390b7482c42a21e73a46dbdc49a5a1c
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-15 11:05:15 +0100

    created dto for author

---
+++ BookStore/Model/DTO/Author/AuthorDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Model.DTO.Author
+{
+    public class AuthorDTO
+    {
+        public string Name { get; set; }
+        public string Biography { get; set; }
+    }
+}
---
+++ BookStore/Model/DTO/Author/AuthorUpdate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Model.DTO.Author
+{
+    public class AuthorUpdate
+    {
+        public Guid AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Biography { get; set; }
+    }
+}
