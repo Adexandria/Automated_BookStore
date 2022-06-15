778bd3d7744fc0f936c5d64cfa069ef847e9d19e
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-15 11:07:34 +0100

    created dto for category

---
+++ BookStore/Model/DTO/Category/CategoryCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Model.DTO.Category
+{
+    public class CategoryCreate
+    {
+        [Required(ErrorMessage ="Enter Faculty")]
+        public string Faculty { get; set; }
+        [Required(ErrorMessage = "Enter Department")]
+        public string Department { get; set; }
+        [Required(ErrorMessage = "Level")]
+        public int Level { get; set; }
+    }
+}
---
+++ BookStore/Model/DTO/Category/CategoryUpdate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Model.DTO.Category
+{
+    public class CategoryUpdate
+    {
+        public Guid CategoryId { get; set; }
+        public string Faculty { get; set; }
+        public string Department { get; set; }
+        public int Level { get; set; }
+    }
+}
