b1385c64cf6f913df48e10ebff75e56fa6c834c1
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-06-20 08:46:55 +0100

    added form properties

--- BookStore/Model/DTO/Book/BookCreate.cs
+++ BookStore/Model/DTO/Book/BookCreate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,8 +10,8 @@ namespace Bookstore.Model.DTO.Book
     {
         public string Name { get; set; }
         public int Price { get; set; }
-        public string Picture { get; set; }
-        public string BookLink { get; set; }
+        public IFormFile Picture { get; set; }
+        public IFormFile BookLink { get; set; }
         public string Department { get; set; }
         public int Level { get; set; }
         public string ISBN { get; set; }
--- BookStore/Model/DTO/Book/BookUpdate.cs
+++ BookStore/Model/DTO/Book/BookUpdate.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,8 +11,8 @@ namespace Bookstore.Model.DTO.Book
         public Guid BookId { get; set; }
         public string Name { get; set; }
         public int Price { get; set; }
-        public string Picture { get; set; }
-        public string BookLink { get; set; }
+        public IFormFile Picture { get; set; }
+        public IFormFile BookLink { get; set; }
         public string Department { get; set; }
         public int Level { get; set; }
         public string ISBN { get; set; }
